f6196be805aa7630df59fb0519d99ff6ed3da212
Vitally Marinchenko <vitalijs.marincenko@accenture.com>
2015-03-26 03:24:55 +0200

    mongoldb added

--- app.js
+++ app.js
@@ -11,8 +11,18 @@ var server = app.listen(3000, function () {
 
   var host = server.address().address;
   var port = server.address().port;
-
   console.log('Example app listening at http://%s:%s', host, port);
 
+  var mongo = require('mongodb').MongoClient
+  var format = require('util').format;
+
+  mongo.connect('mongodb://127.0.0.1:27017/local', function (err, db) {
+    if (err) {
+      throw err;
+    } else {
+      console.log("successfully connected to the database");
+    }
+    db.close();
+  });
 });
 
